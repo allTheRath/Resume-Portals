e2032815fe7fe4a160a635ad90321735b7d05ae0
Keegan Haasbeek <keeganhaasbeek@outlook.com>
2019-10-23 09:56:24 -0500

    -Beginning to work on Seed

---
+++ Models/SeedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace Resume_Portal.Models
+{
+    public class SeedClass
+    {
+        public void SeedStudentUsers(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+
+        }
+    }
+}
